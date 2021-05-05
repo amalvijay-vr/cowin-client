ac79e1d29498a0070f63eb8ae99caa894681e755
mailme.amalvijay <amalvijayvr.developer@gmail.com>
2021-05-05 20:54:02 +0530

    Refactoring Adding Mechanism to check the availability for the next 'n' days

--- src/main/java/com/cowin/client/CoWinClient.java
+++ src/main/java/com/cowin/client/CoWinClient.java
@@ -9,7 +9,11 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,11 +22,28 @@ public class CoWinClient {
     //Basic code to check if there are any slots available for a given date; modify/encapsulate as per your needs
 
     public static void main(String[] args) throws IOException {
-        String date = "06-05-2021";
-        checkAvailabilityFor(date);
+        getNextDays(4)
+                .forEach(CoWinClient::checkAvailabilityFor);
+
+        //To check a single date, use => checkAvailabilityFor("06-05-2021");
     }
 
-    private static void checkAvailabilityFor(String date) throws IOException {
+    private static List<String> getNextDays(int maxDays) {
+        List<String> days = new ArrayList<>();
+        String pattern = "dd-MM-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        Date dt = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(dt);
+        days.add(simpleDateFormat.format(c.getTime()));
+        for (int i = 1; i < maxDays; i++) {
+            c.add(Calendar.DATE, 1);
+            days.add(simpleDateFormat.format(c.getTime()));
+        }
+        return days;
+    }
+
+    private static void checkAvailabilityFor(String date) {
         System.out.println("Availability on " + date);
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
@@ -34,13 +55,33 @@ public class CoWinClient {
                 .addHeader("accept", "application/json")
                 .addHeader("Accept-Language", "hi_IN")
                 .build();
-        Response response = client.newCall(request).execute();
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         if (response.code() == 200) {
             if (response.body() != null) {
-                Centers centers = gson.fromJson(response.body().string(), Centers.class);
+                Centers centers = null;
+                try {
+                    centers = gson.fromJson(response.body().string(), Centers.class);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 centers.getCenters()
                         .forEach(CoWinClient::processCenter);
             }
+        }else {
+            System.out.println("Service not Available");
+            if (response.body() != null) {
+                try {
+                    System.out.println(response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
     }
 
@@ -53,6 +94,8 @@ public class CoWinClient {
         boolean availableSlots = !availableSessions.isEmpty();
         if (availableSlots) {
             handler.handleAvailability(center, availableSessions);
+        } else {
+            handler.handleUnAvailability(center);
         }
     }
 
@@ -63,14 +106,21 @@ public class CoWinClient {
 
 interface AvailabilityHandler {
     void handleAvailability(Center center, List<Session> availableSessions);
+    void handleUnAvailability(Center center);
 }
 
 class PrinterHandler implements AvailabilityHandler {
     @Override
     public void handleAvailability(Center center, List<Session> availableSessions) {
-        Integer slotsCount = availableSessions.stream()
-                .collect(Collectors.summingInt(value -> value.getAvailable_capacity()));
+        int slotsCount = availableSessions.stream()
+                .mapToInt(Session::getAvailable_capacity)
+                .sum();
         System.out.println(Instant.now().toString() + " : "
                 + slotsCount + " slot(s) available in " + center.getBlock_name() + ", " + center.getName());
+    }
+
+    @Override
+    public void handleUnAvailability(Center center) {
+        System.out.println("    ** No Slots **");
     }
 }
